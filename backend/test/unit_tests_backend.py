e8b161ffcc027f17c9420ca3ff3e704e3dc6a2c4
ciskoh <matteo.jriva@gmail.com>
2021-05-11 16:47:58 +0200

    added tests for backend

---
+++ backend/test/unit_tests_backend.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""Unit tests for the funcitons in data"""
+
+import unittest
+import sys
+from pathlib import Path
+sys.path.append(Path("..", "src"))
+
+class Test_q_data(unittest.TestCase):
+
+    def test_retrieve_data(self):
+        #TODO implement this test
+        pass
+    def test_calc_soil_co2(self):
+        # TODO implement this test
+        pass
+    def calc_co_soil_metric(self):
+        # TODO implement this test
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
